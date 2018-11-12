24583eef990c3307c12adfe75984205781f0e6e7
Mohammadreza Zarei <zarei.mohamadreza@gmail.com>
2018-11-12 23:33:04 +0100

    connect the components to the redux

---
+++ src/reducersLoader.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux'
+import { messagesReducer } from './containers/MessageContainer/reducer'
+import { usersReducer } from './containers/UsersList/reducer'
+
+const reducers = combineReducers({
+    messagesReducer,
+    usersReducer
+})
+
+export default reducers
